d2a062203f06999729ea5fc23dcaf2e0b2418762
Siam10101 <mehedihasansiam030@gmail.com>
2022-12-10 10:37:59 +0600

    responsive travel website

---
+++ js/script.js
@@ -0,0 +1,95 @@
+let navbar = document.querySelector(".header .navbar");
+
+document.querySelector("#menu-btn").onclick = () => {
+  navbar.classList.add("active");
+};
+
+document.querySelector("#nav-close").onclick = () => {
+  navbar.classList.remove("active");
+};
+
+let searchForm = document.querySelector(".search-form");
+
+document.querySelector("#search-btn").onclick = () => {
+  searchForm.classList.add("active");
+};
+
+document.querySelector("#close-search").onclick = () => {
+  searchForm.classList.remove("active");
+};
+
+window.onscroll = () => {
+  navbar.classList.remove("active");
+
+  if (window.scrollY > 0) {
+    document.querySelector(".header").classList.add("active");
+  } else {
+    document.querySelector(".header").classList.remove("active");
+  }
+};
+
+window.onload = () => {
+  if (window.scrollY > 0) {
+    document.querySelector(".header").classList.add("active");
+  } else {
+    document.querySelector(".header").classList.remove("active");
+  }
+};
+
+/*=============== HOME SWIPER ===============*/
+var swiper = new Swiper(".home-slider", {
+  loop: true,
+  grabCursor: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+/*=============== ABOUT VIDEO===============*/
+document
+  .querySelectorAll(" .about .about--video .controls .control-btn")
+  .forEach((btn) => {
+    btn.onclick = () => {
+      let src = btn.getAttribute("data-src");
+      document.querySelector(".about .about--video .video").src = src;
+    };
+  });
+/*=============== REVIEW SWIPER ===============*/
+var swiper = new Swiper(".review-slider", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 20,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+/*=============== CLIENT SWIPER ===============*/
+var swiper = new Swiper(".clients-slider", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 20,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+  },
+});
